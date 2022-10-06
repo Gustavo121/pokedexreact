e1da71acb6b65386b1293c4b4e397168d7cab63d
Gustavo <gustgouvea@hotmail.com>
2022-10-06 11:08:25 -0300

    colocando nav , input e button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Searchbar from "./components/Searchbar";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Searchbar/>
       <div className="App"></div>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,13 @@
 import React from "react";
 
 const Navbar = () => {
+  const logoImg =
+    "https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png";
   return (
     <nav>
-      <div>Pokeapi</div>
+      <div>
+        <img alt="pokeapi-logo" src={logoImg} className="navbar-img" />
+      </div>
     </nav>
   );
 };
